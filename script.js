7d9e3227de0f40585d387170d2439f683fd24b53
tjonesdev <tracey.l.jones@outlook.com>
2020-08-16 12:20:37 -0500

    uploading to github

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  // Screen Var
+  var screen = document.getElementById("screen");
+  
+  // Control Vars
+  var clear = document.getElementById("clear");
+  var equals = document.getElementById("equals");
+  var del = document.getElementById("del");
+  
+  // Math Vars
+  var percent = document.getElementById("percent");
+  var divide = document.getElementById("divide");
+  var multiply = document.getElementById("multiply");
+  var minus = document.getElementById("minus");
+  var plus = document.getElementById("plus");
+  var plusMinus = document.getElementById("plusminus");
+  var decimal = document.getElementById("decimal");
+  
+  // Number Vars
+  var num9 = document.getElementById("num9");
+  var num8 = document.getElementById("num8");
+  var num7 = document.getElementById("num7");
+  var num6 = document.getElementById("num6");
+  var num5 = document.getElementById("num5");
+  var num4 = document.getElementById("num4");
+  var num3 = document.getElementById("num3");
+  var num2 = document.getElementById("num2");
+  var num1 = document.getElementById("num1");
+  var num0 = document.getElementById("num0");
+});
